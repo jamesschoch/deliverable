ac03a70155ed2ad02cfc40e43f1f18dc48811692
cyberbyteNZ <33851917+cyberbyteNZ@users.noreply.github.com>
2020-11-07 17:42:00 +1300

    v3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 window.SS = window.SS || {};
 SS.main = SS.main || {};
-
+var enableRotate = 0;
 SS.main.main = function() {
 	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
-	renderer.setClearColor(0x000000, 1);
+	renderer.setClearColor(0x000000, 0);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.domElement.setAttribute('id', 'renderer');
 	document.body.appendChild(renderer.domElement);
@@ -13,7 +13,7 @@ SS.main.main = function() {
 	scene = new THREE.Scene();
 	var ratio = renderer.getContext().drawingBufferWidth / renderer.getContext().drawingBufferHeight;
 	camera = new THREE.PerspectiveCamera(60, ratio, 0.1, 10000);
-	editorCamera = new SS.util.EditorCamera(camera, document, 15, new THREE.Vector2(-Math.PI*(2/4),-Math.PI*(1/4)));
+	editorCamera = new SS.util.EditorCamera(camera, document, 15, new THREE.Vector2(0, 0));
 
 	SS.util.addResizeListener();
 	SS.main.addSceneContent(scene);
@@ -27,11 +27,19 @@ SS.main.render = function() {
 	time = window.time || new Date().getTime();
 	var newTime = new Date().getTime();
 	var diff = newTime - time;
-	if (editorCamera.mouseDown == false) {
+	if (enableRotate) {
+		// console.log("making frames");
+		editorCamera.cameraPos.y += 0;
 		editorCamera.cameraPos.x += diff/3000;
+		editorCamera.cameraPos.z += 0;
 		editorCamera.cameraStartPos = editorCamera.cameraPos;
-		//editorCamera.rotateCamera();
-	}
+			editorCamera.rotateCamera();
+	} else {
+
+	editorCamera.cameraStartPos = editorCamera.cameraPos;
+		editorCamera.rotateCamera();
+}
+
 	time = newTime;
 
 	renderer.render(scene, camera);
@@ -39,7 +47,7 @@ SS.main.render = function() {
 
 SS.main.addSceneContent = function(scene) {
 	sunLight = new THREE.PointLight(new THREE.Color(0xffffff), 1.0);
-	sunLight.position.set(100, 0, 0);
+	sunLight.position.set(50, 0, 0);
 	scene.add(sunLight);
 
 	scene.add(new SS.planet.Planet(5));
--- js/util.js
+++ js/util.js
@@ -1,100 +1,117 @@
 window.SS = window.SS || {};
 SS.util = SS.util || {};
-
+var newy = 0;
+var style = "entryspin";
 SS.util.clamp = function(number, from, to) {
-	return Math.max(Math.min(number, to), from);
-}
+  return Math.max(Math.min(number, to), from);
+};
 
 SS.util.randomInt = function(from, to, seed) {
-	return Math.floor(SS.util.randomFloat(from, to+1, seed));
-}
+  return Math.floor(SS.util.randomFloat(from, to + 1, seed));
+};
 
 SS.util.randomFloat = function(from, to, seed) {
-	return SS.util.random(seed)*(to-from)+from;
-}
+  return SS.util.random(seed) * (to - from) + from;
+};
 
 SS.util.random = function(seed) {
-	var scope = arguments.callee;
+  var scope = arguments.callee;
 
-	scope.MAX = scope.MAX || Math.pow(2, 32);
-	scope.a = 1664525;
-	scope.c = 1013904223;
+  scope.MAX = scope.MAX || Math.pow(2, 32);
+  scope.a = 1664525;
+  scope.c = 1013904223;
 
-	scope.seeds = scope.seeds || {};
+  scope.seeds = scope.seeds || {};
 
-	seed = seed || 0;
-	var key = seed;
-	if (typeof seed == "string") {
-		if (scope.seeds[seed] == undefined) {
-			var numeric = SS.util.numberFromString(seed);
-			scope.seeds[seed] = numeric; // Memoization
-			seed = numeric;
-		} else {
-			seed = scope.seeds[seed];
-		}
-	}
-	scope.series = scope.series || {};
-	scope.series[key] = scope.series[key] || seed;
+  seed = seed || 0;
+  var key = seed;
+  if (typeof seed == "string") {
+    if (scope.seeds[seed] == undefined) {
+      var numeric = SS.util.numberFromString(seed);
+      scope.seeds[seed] = numeric; // Memoization
+      seed = numeric;
+    } else {
+      seed = scope.seeds[seed];
+    }
+  }
+  scope.series = scope.series || {};
+  scope.series[key] = scope.series[key] || seed;
 
-	var lastRandom = scope.series[key];
-	var newRandom = (scope.a * lastRandom + scope.c) % scope.MAX;
+  var lastRandom = scope.series[key];
+  var newRandom = (scope.a * lastRandom + scope.c) % scope.MAX;
 
-	scope.series[key] = newRandom;
+  scope.series[key] = newRandom;
 
-	return newRandom/scope.MAX;
-}
+  return newRandom / scope.MAX;
+};
 
 SS.util.resetRandomSeries = function(prefix) {
-	var toBeCleared = [];
-	for (var i in SS.util.random.series) {
-		if (i.indexOf(prefix) == 0) toBeCleared.push(i);
-	}
-	for (var i in toBeCleared) {
-		delete SS.util.random.series[toBeCleared[i]];
-	}
-}
+  var toBeCleared = [];
+  for (var i in SS.util.random.series) {
+    if (i.indexOf(prefix) == 0) toBeCleared.push(i);
+  }
+  for (var i in toBeCleared) {
+    delete SS.util.random.series[toBeCleared[i]];
+  }
+};
 
 SS.util.makeSpecifiedArray1D = function(size, value, array) {
-	var valueFloat = value;
-	for (var x = 0; x < size; x++) {
-		if (typeof(value) == "function") valueFloat = value(x);
-		array[x] = valueFloat;
-	}
-	return array;
-}
+  var valueFloat = value;
+  for (var x = 0; x < size; x++) {
+    if (typeof value == "function") valueFloat = value(x);
+    array[x] = valueFloat;
+  }
+  return array;
+};
 
-window.N = 256*256;
+window.N = 256 * 256;
 window.G = SS.util.makeSpecifiedArray1D(N, Math.random, new Float32Array(N));
-window.P = SS.util.makeSpecifiedArray1D(N, function() {return SS.util.randomInt(0, N-1)}, new Uint32Array(N));
+window.P = SS.util.makeSpecifiedArray1D(
+  N,
+  function() {
+    return SS.util.randomInt(0, N - 1);
+  },
+  new Uint32Array(N)
+);
 
 SS.util.random4 = function(i, j, k) {
-	return G[(i + P[(j + P[k % N]) % N]) % N];
-}
+  return G[(i + P[(j + P[k % N]) % N]) % N];
+};
 
-SS.util.EditorCamera = function(camera, document, startRadius, cameraStartPos, originObject) {
-	this.camera = camera;
-	this.mouseDown = false;
-	this.startRadius = startRadius || 20;
-	this.startExp = 6;
-	this.radius = this.startExp;
-	this.originObject = originObject || {position: new THREE.Vector3()};
-	this.cameraStartPos = cameraStartPos || new THREE.Vector2(Math.PI/8, -Math.PI/4);
-	this.cameraPos = this.cameraStartPos.clone();
-	this.mouseClickPos = new THREE.Vector2();
+SS.util.EditorCamera = function(
+  camera,
+  document,
+  startRadius,
+  cameraStartPos,
+  originObject
+) {
+  this.camera = camera;
+  this.mouseDown = false;
+  this.startRadius = startRadius || 20;
+  this.startExp = 6;
+  this.radius = this.startExp;
+  this.originObject = originObject || { position: new THREE.Vector3() };
+  this.cameraStartPos =
+    cameraStartPos || new THREE.Vector2(Math.PI / 8, -Math.PI / 4);
+  this.cameraPos = this.cameraStartPos.clone();
+  this.mouseClickPos = new THREE.Vector2();
 
-	var editorCamera = this;
+  var editorCamera = this;
 
-	var addEventListeners = function() {
-		document.addEventListener('mousemove', function(event) {
-			var mousePos = new THREE.Vector2(event.clientX, event.clientY);
-			if (editorCamera.mouseDown == true) {
-				var diff = mousePos.clone().sub(editorCamera.mouseClickPos).multiplyScalar(1/250);
-				editorCamera.cameraPos = editorCamera.cameraStartPos.clone().add(diff);
-				editorCamera.rotateCamera();
-			}
-		});
-
-		/* document.addEventListener('mousewheel', function(event) {
+  var addEventListeners = function() {
+    document.addEventListener("mousemove", function(event) {
+      var mousePos = new THREE.Vector2(event.clientX, event.clientY);
+      if (editorCamera.mouseDown == true) {
+        var diff = mousePos
+          .clone()
+          .sub(editorCamera.mouseClickPos)
+          .multiplyScalar(1 / 500);
+        editorCamera.cameraPos = editorCamera.cameraStartPos.clone().add(diff);
+        editorCamera.rotateCamera();
+      }
+    });
+    /*
+		 document.addEventListener('mousewheel', function(event) {
 			var delta = event.wheelDelta/10000;
 			if (editorCamera.getScaledRadius(editorCamera.radius - delta) >= 0) {
 				editorCamera.radius -= delta;
@@ -102,140 +119,229 @@ SS.util.EditorCamera = function(camera, document, startRadius, cameraStartPos, o
 			}
 		}); */
 
-		document.addEventListener('mousedown', function(event) {
-			var mousePos = new THREE.Vector2(event.clientX, event.clientY);
-			editorCamera.mouseClickPos = mousePos;
-			editorCamera.mouseDown = true;
-		});
+    document.addEventListener("mousedown", function(event) {
+      var mousePos = new THREE.Vector2(event.clientX, event.clientY);
+      editorCamera.mouseClickPos = mousePos;
+      editorCamera.mouseDown = true;
+    });
 
-		document.addEventListener('mouseup', function(event) {
-			editorCamera.cameraStartPos = editorCamera.cameraPos;
-			editorCamera.mouseDown = false;
-		});
-	}
+    document.addEventListener("mouseup", function(event) {
+      editorCamera.cameraStartPos = editorCamera.cameraPos;
+      editorCamera.mouseDown = false;
+    });
+  };
+
+  this.getScaledRadius = function(radius) {
+    return Math.exp(radius) - Math.exp(this.startExp) + this.startRadius;
+  };
+
+  this.zoomCamera = function() {
+    this.camera.position
+      .normalize()
+      .multiplyScalar(this.getScaledRadius(this.radius));
+  };
+
+  /*
+	function setCameraToRotate() {
+		editorCamera.camera.position.y = Math.sin((this.cameraPos.x + Math.PI/2));
+		editorCamera.camera.position.x = 1.5 * (Math.cos((this.cameraPos.x + Math.PI/2) * 2)/10) -0.85;
+		editorCamera.camera.position.z = 0.7 * (-1 * Math.cos((this.cameraPos.x + Math.PI/2)));
+
+		editorCamera.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+		editorCamera.camera.position.add(editorCamera.originObject.position);
+	};
 
 
-	this.getScaledRadius = function(radius) {
-		return Math.exp(radius) - Math.exp(this.startExp) + this.startRadius
-	}
+	function setCameraToStartPosition() {
+		editorCamera.camera.position.y = 0;
+		editorCamera.camera.position.x = -0.7;
+		editorCamera.camera.position.z = -0.7;
 
-	this.zoomCamera = function() {
-		this.camera.position.normalize().multiplyScalar(this.getScaledRadius(this.radius));
-	}
+		editorCamera.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-	this.rotateCamera = function() {
-		this.camera.position.y = -this.cameraPos.y;
-		this.camera.position.x = Math.sin(this.cameraPos.x);
-		this.camera.position.z = Math.cos(this.cameraPos.x);
+		editorCamera.camera.position.add(editorCamera.originObject.position);
+	}; */
 
-		this.zoomCamera();
+  this.rotateCamera = function() {
+		if (style == "newspin"){
+		// New Spin mechanic
+    this.camera.position.y = Math.sin((this.cameraPos.x + Math.PI/2));
+    this.camera.position.x = 1.5 * (Math.cos((this.cameraPos.x + Math.PI/2) * 2)/10) -0.85;
+    this.camera.position.z = 0.7 * (-1 * Math.cos((this.cameraPos.x + Math.PI/2)));
+		// console.log("newspin");
 
-		this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+} else if (style == "classicspin"){
 
-		this.camera.position.add(this.originObject.position);
-	}
+		// Classic Spin mechanic
+    this.camera.position.y = -this.cameraPos.y;
+    this.camera.position.x = Math.sin(this.cameraPos.x);
+    this.camera.position.z = Math.sin(this.cameraPos.x);
+// console.log("classicspin");
 
-	addEventListeners();
-	this.rotateCamera();
-}
+} else if (style == "entryspin"){
+		// New Entry mechanic
+		if (newy >= 0.99) {
+			newy = 1
+		} else {
+		newy = ((Math.sin((this.cameraPos.x + Math.PI/2))+1)/10*7)*-1+1;
+		if (newy >= 0.99) {
+			newy = 1
+		}};
+		// console.log("entryspin");
+    this.camera.position.y = newy;
+    this.camera.position.x = -1;
+    this.camera.position.z = 0;
+
+	};
+
+    this.zoomCamera();
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    this.camera.position.add(this.originObject.position);
+  };
+
+  addEventListeners();
+  this.rotateCamera();
+};
 
 SS.util.addResizeListener = function() {
-	window.addEventListener('resize', function() {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-	});
-}
+  window.addEventListener("resize", function() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+};
 
 SS.util.computeGeometry = function(geometry) {
-	geometry.makeGroups();
-	geometry.computeVertexNormals()
-	geometry.computeFaceNormals();
-	geometry.computeMorphNormals();
-	geometry.computeBoundingSphere();
-	geometry.computeBoundingBox();
-	geometry.computeLineDistances();
+  geometry.makeGroups();
+  geometry.computeVertexNormals();
+  geometry.computeFaceNormals();
+  geometry.computeMorphNormals();
+  geometry.computeBoundingSphere();
+  geometry.computeBoundingBox();
+  geometry.computeLineDistances();
 
-	geometry.verticesNeedUpdate = true;
-	geometry.elementsNeedUpdate = true;
-	geometry.uvsNeedUpdate = true;
-	geometry.normalsNeedUpdate = true;
-	geometry.tangentsNeedUpdate = true;
-	geometry.colorsNeedUpdate = true;
-	geometry.lineDistancesNeedUpdate = true;
-	geometry.buffersNeedUpdate = true;
-	geometry.groupsNeedUpdate = true;
-}
+  geometry.verticesNeedUpdate = true;
+  geometry.elementsNeedUpdate = true;
+  geometry.uvsNeedUpdate = true;
+  geometry.normalsNeedUpdate = true;
+  geometry.tangentsNeedUpdate = true;
+  geometry.colorsNeedUpdate = true;
+  geometry.lineDistancesNeedUpdate = true;
+  geometry.buffersNeedUpdate = true;
+  geometry.groupsNeedUpdate = true;
+};
 
-SS.util.trilinearInterpolation = function(coordFloat, scalarField, interpolation) {
-	interpolation = interpolation || function(a, b, x) {
-		return  a*(1-x) + b*x;
-	}
+SS.util.trilinearInterpolation = function(
+  coordFloat,
+  scalarField,
+  interpolation
+) {
+  interpolation =
+    interpolation ||
+    function(a, b, x) {
+      return a * (1 - x) + b * x;
+    };
 
-	var coord0 = {x: Math.floor(coordFloat.x), y: Math.floor(coordFloat.y), z: Math.floor(coordFloat.z)};
-	var coord1 = {x: coord0.x+1, y: coord0.y+1, z: coord0.z+1};
-	var xd = (coordFloat.x - coord0.x)/Math.max(1, (coord1.x-coord0.x));
-	var yd = (coordFloat.y - coord0.y)/Math.max(1, (coord1.y-coord0.y));
-	var zd = (coordFloat.z - coord0.z)/Math.max(1, (coord1.z-coord0.z));
-	var c00 = interpolation(scalarField(coord0.x, coord0.y, coord0.z), scalarField(coord1.x, coord0.y, coord0.z), xd);
-	var c10 = interpolation(scalarField(coord0.x, coord1.y, coord0.z), scalarField(coord1.x, coord1.y, coord0.z), xd);
-	var c01 = interpolation(scalarField(coord0.x, coord0.y, coord1.z), scalarField(coord1.x, coord0.y, coord1.z), xd);
-	var c11 = interpolation(scalarField(coord0.x, coord1.y, coord1.z), scalarField(coord1.x, coord1.y, coord1.z), xd);
-	var c0 = interpolation(c00, c10, yd);
-	var c1 = interpolation(c01, c11, yd);
-	var c = interpolation(c0, c1, zd);
+  var coord0 = {
+    x: Math.floor(coordFloat.x),
+    y: Math.floor(coordFloat.y),
+    z: Math.floor(coordFloat.z)
+  };
+  var coord1 = { x: coord0.x + 1, y: coord0.y + 1, z: coord0.z + 1 };
+  var xd = (coordFloat.x - coord0.x) / Math.max(1, coord1.x - coord0.x);
+  var yd = (coordFloat.y - coord0.y) / Math.max(1, coord1.y - coord0.y);
+  var zd = (coordFloat.z - coord0.z) / Math.max(1, coord1.z - coord0.z);
+  var c00 = interpolation(
+    scalarField(coord0.x, coord0.y, coord0.z),
+    scalarField(coord1.x, coord0.y, coord0.z),
+    xd
+  );
+  var c10 = interpolation(
+    scalarField(coord0.x, coord1.y, coord0.z),
+    scalarField(coord1.x, coord1.y, coord0.z),
+    xd
+  );
+  var c01 = interpolation(
+    scalarField(coord0.x, coord0.y, coord1.z),
+    scalarField(coord1.x, coord0.y, coord1.z),
+    xd
+  );
+  var c11 = interpolation(
+    scalarField(coord0.x, coord1.y, coord1.z),
+    scalarField(coord1.x, coord1.y, coord1.z),
+    xd
+  );
+  var c0 = interpolation(c00, c10, yd);
+  var c1 = interpolation(c01, c11, yd);
+  var c = interpolation(c0, c1, zd);
 
-	return c;
-}
+  return c;
+};
 
 SS.util.nearestNeighbour = function(coordFloat, scalarField) {
-	return scalarField(Math.floor(coordFloat.x), Math.floor(coordFloat.y), Math.floor(coordFloat.z));
-}
+  return scalarField(
+    Math.floor(coordFloat.x),
+    Math.floor(coordFloat.y),
+    Math.floor(coordFloat.z)
+  );
+};
 
 SS.util.tricosineInterpolation = function(coordFloat, scalarField) {
-	var interpolation = function(a, b, x) {
-		var ft = x * 3.1415927;
-		var f = (1 - Math.cos(ft)) * 0.5;
-		return  a*(1-f) + b*f
-	}
+  var interpolation = function(a, b, x) {
+    var ft = x * 3.1415927;
+    var f = (1 - Math.cos(ft)) * 0.5;
+    return a * (1 - f) + b * f;
+  };
 
-	return SS.util.trilinearInterpolation(coordFloat, scalarField, interpolation);
-}
+  return SS.util.trilinearInterpolation(coordFloat, scalarField, interpolation);
+};
 
 SS.util.heightToNormalMap = function(map, intensity) {
-    var width = map.image.width;
-	var height = map.image.height;
-	var nofPixels = width*height;
+  var width = map.image.width;
+  var height = map.image.height;
+  var nofPixels = width * height;
 
-    intensity = intensity || 1.0;
+  intensity = intensity || 1.0;
 
-    var getHeight = function(x, y) {
-        x = Math.min(x, width-1);
-        y = Math.min(y, height-1);
-        return (
-            map.image.data[(y*width+x)*3+0]/255 +
-            map.image.data[(y*width+x)*3+1]/255 +
-            map.image.data[(y*width+x)*3+2]/255
-        )/3*intensity;
-    }
+  var getHeight = function(x, y) {
+    x = Math.min(x, width - 1);
+    y = Math.min(y, height - 1);
+    return (
+      ((map.image.data[(y * width + x) * 3 + 0] / 255 +
+        map.image.data[(y * width + x) * 3 + 1] / 255 +
+        map.image.data[(y * width + x) * 3 + 2] / 255) /
+        3) *
+      intensity
+    );
+  };
 
-    var normalMap = THREE.ImageUtils.generateDataTexture(width, height, new THREE.Color(0x000000));
+  var normalMap = THREE.ImageUtils.generateDataTexture(
+    width,
+    height,
+    new THREE.Color(0x000000)
+  );
 
-	for (var i = 0; i < nofPixels; i++) {
-		var x = i%width;
-		var y = Math.floor(i/width);
+  for (var i = 0; i < nofPixels; i++) {
+    var x = i % width;
+    var y = Math.floor(i / width);
 
-        var pixel00 = new THREE.Vector3(0, 0, getHeight(x, y));
-        var pixel01 = new THREE.Vector3(0, 1, getHeight(x, y+1));
-        var pixel10 = new THREE.Vector3(1, 0, getHeight(x+1, y));
-        var orto = pixel10.clone().sub(pixel00).cross(pixel01.clone().sub(pixel00)).normalize();
+    var pixel00 = new THREE.Vector3(0, 0, getHeight(x, y));
+    var pixel01 = new THREE.Vector3(0, 1, getHeight(x, y + 1));
+    var pixel10 = new THREE.Vector3(1, 0, getHeight(x + 1, y));
+    var orto = pixel10
+      .clone()
+      .sub(pixel00)
+      .cross(pixel01.clone().sub(pixel00))
+      .normalize();
 
-		var color = new THREE.Color(orto.x+0.5, orto.y+0.5, -orto.z);
+    var color = new THREE.Color(orto.x + 0.5, orto.y + 0.5, -orto.z);
 
-        normalMap.image.data[i*3+0] = color.r*255;
-		normalMap.image.data[i*3+1] = color.g*255;
-		normalMap.image.data[i*3+2] = color.b*255;
-    }
+    normalMap.image.data[i * 3 + 0] = color.r * 255;
+    normalMap.image.data[i * 3 + 1] = color.g * 255;
+    normalMap.image.data[i * 3 + 2] = color.b * 255;
+  }
 
-    return normalMap;
-}
+  return normalMap;
+};
